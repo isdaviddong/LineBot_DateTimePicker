597cfb0b87a2da28b6e47fde3ceb4455ddce7e87
David@SurfacePro4 <isdaviddong@gmail.com>
2017-09-28 15:58:38 +0800

    v1

---
+++ WebApplication1/Controllers/LinebotController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class LinebotController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult POST()
+        {
+            string ChannelAccessToken = "!!!!!!!!!!請改用自己的Line bot Token!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message = "";
+                if (ReceivedMessage.events[0].message != null && !string.IsNullOrEmpty(ReceivedMessage.events[0].message.text))
+                    Message = "你說了:" + ReceivedMessage.events[0].message.text;
+
+                if (ReceivedMessage.events[0].postback != null)
+                {
+                    Message += "收到 postback data " + ReceivedMessage.events[0].postback.data;
+                    Message += "\n postback params(datetime) " + 
+                        ReceivedMessage.events[0].postback.Params.datetime;
+                    Message += "\n postback params(date) " +
+                        ReceivedMessage.events[0].postback.Params.date;
+                    Message += "\n postback params(time) " + 
+                        ReceivedMessage.events[0].postback.Params.time;
+                }
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(
+                    ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //請自行handle Exception
+                return Ok();
+            }
+        }
+    }
+}
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //建立Bot instance
+            isRock.LineBot.Bot bot =
+                new isRock.LineBot.Bot("!!!!!!!!!!請改用自己的Line bot Token!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");  //傳入Channel access token
+
+            //建立actions，作為ButtonTemplate的用戶回覆行為
+            var actions = new List<isRock.LineBot.TemplateActionBase>();
+            actions.Add(new isRock.LineBot.DateTimePickerAction() {
+                label = "測試-選取時間", mode = "time" });
+            actions.Add(new isRock.LineBot.DateTimePickerAction() {
+                label = "測試-選取日期", mode = "date" });
+            actions.Add(new isRock.LineBot.DateTimePickerAction() {
+                label = "測試-選取時間日期", mode = "datetime" });
+
+            //單一Button Template Message
+            var ButtonTemplate = new isRock.LineBot.ButtonsTemplate()
+            {
+                text = "這個範例測試使用Line新釋出的DateTime Action，讓用戶選擇時間日期並取得會傳值...",
+                title = "ButtonsTemplate測試",
+                //設定圖片
+                thumbnailImageUrl = new Uri("https://arock.blob.core.windows.net/blogdata201706/22-124357-ad3c87d6-b9cc-488a-8150-1c2fe642d237.png"),
+                actions = actions //設定回覆動作
+            };
+            //發送
+            bot.PushMessage("!!!!!!!!!!請改用自己的Line bot Admin User ID!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", ButtonTemplate);
+        }
+    }
+}
